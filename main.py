4a977a153b34e1dce613aa439776c9ffc55a731d
Faiza <abed.faiza2000@gmail.com>
2025-10-16 10:09:11 +0100

    Le conflit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from modules.utils import hello
 from modules.math.tools import multiply
+from modules.calculator import add
 print ("Exam in git")
 print (hello("Git user"))
 print ("Résultat du calcul :", multipy(4, 5))
+print ("Somme:" , add(10, 5))
+print ("Programme executé avec succés")
